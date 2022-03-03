d0a2f3755c42e54d552a2c1000997dc2e669e439
Eric Brusky <bruskyeric@gmail.com>
2022-03-02 20:00:19 -0600

    initial commit

---
+++ Loop Challenges/script.js
@@ -0,0 +1,43 @@
+// Some loops challenges
+
+// 1. Print the Odds
+
+// for(var i = 1; i < 21; i ++) {
+//     if(i % 2 == 1) {
+//         console.log(i);
+//     }
+// }
+
+// 2. Decreasing Multiples of 3
+
+// for(var i = 100; i >= 0; i --) {
+//     if(i % 3 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 3. Print the sequence
+
+// for(var i = 4; i > -4; i -= 1.5) {
+//     console.log(i);
+// }
+
+// 4. Sigma
+
+// var count = 1;
+// var sum = 0;
+// while(count < 101) {
+//     sum += count;    
+//     count ++;
+//     console.log(sum);
+// }
+
+//5. Factorial
+
+// var count = 1;
+// var product = 1;
+// while (count < 13) {
+//     product *= count;
+//     count ++;
+//     console.log(product);
+// }
